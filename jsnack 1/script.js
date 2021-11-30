29cd10c161c1ce0f2d93a9d3a63b8fe2405db4de
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-30 10:42:38 +0100

    ciclo for snack 1

--- jsnack 1/script.js
+++ jsnack 1/script.js
@@ -1,1 +1,17 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+
+// presi due array per nomi e cognomi
+let nomi =  ['Giuseppe', 'Carmelo', 'Giovanni', 'Rosa', 'Maria', 'Francesco', 'Daniela', 'Fulvio', 'Alessio', 'Andrea'];
+let cognomi = ['Bianchi', 'Rossi', 'Verdi', 'Mattarella', 'Obama', 'Russo', 'Vinci', 'Buonarroti', 'Monet', 'Wilde'];
+
+// array vuoto per lista generata
+let ospiti = [];
+
+// genero lista di 10 ospiti
+
+for (let index = 0; index < 10; index++) {
+    let ospite = nomi[Math.floor(Math.random() * 9)] + ' ' + cognomi[Math.floor(Math.random() * 9)];
+    ospiti.push(ospite);
+}
+// stampo in console
+console.log(ospiti)
