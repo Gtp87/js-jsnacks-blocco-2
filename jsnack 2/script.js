8e16fe1d2a530088c14793c8519a8dd2bfded921
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-30 11:24:16 +0100

    end snack2

--- jsnack 2/script.js
+++ jsnack 2/script.js
@@ -1,1 +1,25 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+// dichiaro array
+let arrNum = [3, 5, 72, 38, 105, 1002, 58, 90, 1, 22, 38];
+let sum = 0;
+console.log(arrNum);
+
+// presa lugnhezza array
+
+for (let index = 0; index < arrNum.length; index++) {
+    
+    // se la posizione de index e' pari
+    if (index % 2 == 0) {
+        console.log('numero in posizione pari', arrNum[index]);
+
+    // altrimenti se e' dispari
+    } else {
+        console.log('numero in posizione dispari', arrNum[index]);
+        sum += arrNum[index];
+        
+    }
+      
+}
+
+console.log('somma numeri posizione dispari: ' + sum);
